93a231f953ecebbd2fc1f9b108855905c2c62bf5
Abdellah1068695 <1068695@pdsb.net>
2024-12-04 10:04:25 -0500

    Update Driver code.py

--- Driver code.py
+++ Driver code.py
@@ -4,15 +4,29 @@ from poly import *
 def getNumeric(S : str):
     # input: S is a point in the format "(x,y)" (type str)
     # output: a tuple or list indicating a point (x, y) where x, y are int or float
-    x = None
-    y = None # obiviously, change these
+    P = S.strip("()")
+    [x,y] = P.split(",")
+    try:
+        x = int(x)
+        
+    except:
+        x = float(x)
+        
+    try:
+        y = int(y)
+        
+    except:
+        y = float(y)
+    #x = None
+    #y = None # obiviously, change these
 
     return (x, y)
 
 fh = open("a2.txt", "r") # this is the name of the data file to open
 
 polydata = fh.readline().strip()
-
+poly = getNumeric(polydata)
+Poly = point(x,y)
 # make an array of points (str)
 # declare a polygon
 # loop through the points array and turn them into numbers for the polynomial object
