266c2c400a15a812fee9eb9a6e122f8f16cb60a5
Abdellah1068695 <1068695@pdsb.net>
2025-01-13 10:53:56 -0800

    Update polydriver.py

--- polydriver.py
+++ polydriver.py
@@ -1,7 +1,18 @@
 from Polynomial import Polynomial
-# driver
-P = Polynomial([0, 0, 1, 2, 0, 3, 0, 0])
-print(P)
-for i in range(10):
-    print(i, P.f(i))
-print(P.get_order())
+
+def get_coefficients():
+    coeffs = input("Enter the coefficients of the polynomial separated by commas: ")
+    return list(map(float, coeffs.split(',')))
+
+# Get coefficients from user
+coefficients = get_coefficients()
+
+# Define a polynomial
+p5 = Polynomial(coefficients)
+print("Polynomial:", p5)
+
+# Print the order of the polynomial
+print("Order of the polynomial:", p5.get_order())
+
+# Evaluate the polynomial at x = 2
+print("Value of the polynomial at x = 2:", p5.f(2))
